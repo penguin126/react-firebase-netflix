3d8bea3c437ebbee74be5bf5dadc3c7ce3eeb0a8
vinncyy <viincyy126@gmail.com>
2021-04-08 21:28:30 +0700

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      imgURL:
+        "https://d2.alternativeto.net/dist/listheaders//list-4097-1530878541-cover.png?format=jpg&width=1520&height=760&mode=crop",
+      username: "Justifiable",
+      caption: "What a beautiful day!",
+    },
+    {
+      imgURL: "https://sandilands.info/sgordon/images/math-example.png",
+      username: "viincyy",
+      caption: "How can I do it?",
+    },
+    {
+      imgURL:
+        "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
+      username: "itdev126",
+      caption: "Ahm boiz!!!",
+    },
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -12,7 +32,13 @@ function App() {
           alt=""
         />
       </div>
-      <Post />
+      {posts.map((post) => (
+        <Post
+          imgURL={post.imgURL}
+          caption={post.caption}
+          username={post.username}
+        />
+      ))}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./Post.css";
-function Post() {
+import Avatar from "@material-ui/core/Avatar";
+
+function Post({ imgURL, caption, username }) {
   return (
     <div className="container">
-      <h3>Justifiable</h3>
-      {/* images */}
-      <img
-        className="post__image"
-        src="https://images.unsplash.com/photo-1523478482487-1eed2b3d9939?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDIwfHxsb2FkfGVufDB8fHx8MTYxNTgxODMwOA&ixlib=rb-1.2.1&q=80&w=2000 300w,
-                            https://images.unsplash.com/photo-1523478482487-1eed2b3d9939?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDIwfHxsb2FkfGVufDB8fHx8MTYxNTgxODMwOA&ixlib=rb-1.2.1&q=80&w=2000 600w,
-                            https://images.unsplash.com/photo-1523478482487-1eed2b3d9939?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDIwfHxsb2FkfGVufDB8fHx8MTYxNTgxODMwOA&ixlib=rb-1.2.1&q=80&w=2000 1000w,
-                            https://images.unsplash.com/photo-1523478482487-1eed2b3d9939?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDIwfHxsb2FkfGVufDB8fHx8MTYxNTgxODMwOA&ixlib=rb-1.2.1&q=80&w=2000 2000w"
-        alt=""
-      />
-      {/* uername + caption */}
-      <p className="post__text">
-        <strong>Justifiable</strong> What a beautiful day!
-      </p>
+      <div className="post">
+        <div className="post__header">
+          <Avatar
+            className="post__avatar"
+            alt="Justifiable"
+            src="/static/images/1.jpg"
+          />
+          <h3>{username}</h3>
+        </div>
+        {/* images */}
+        <img className="post__image" src={imgURL} alt="" />
+        {/* uername + caption */}
+        <p className="post__text">
+          <strong>{username}</strong> {caption}
+        </p>
+      </div>
     </div>
   );
 }
