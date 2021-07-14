093c0bc85a4371e8bb0fccc69f9771ff4469f101
viincyy <viincyy126@gmail.com>
2021-07-14 21:09:35 +0700

    clean project repos

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Post from "./Post";
-import { db } from "./firebase";
-function App() {
-  const [posts, setPosts] = useState([]);
+import React from "react";
 
-  useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => doc.data()));
-    });
-  }, []);
+export default function App() {
   return (
-    <div className="app">
-      <div className="app__header">
-        <img
-          className="app__headerImg"
-          src="https:www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt=""
-        />
-      </div>
-      {posts.map((post) => (
-        <Post
-          imgURL={post.imgURL}
-          caption={post.caption}
-          username={post.username}
-        />
-      ))}
-    </div>
+    <p>Hello</p>
   );
 }
-
-export default App;
