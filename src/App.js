058eb5228c9d3d03545120ea7b440dcb8f39b943
viincyy <viincyy126@gmail.com>
2021-07-15 13:08:51 +0700

    handle opt-form && accordion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { JumbotronContaier } from "./containers/jumbotron";
 import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
   return (
     <>
       <JumbotronContaier />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
